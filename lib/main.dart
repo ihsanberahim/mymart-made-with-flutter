d5ed96366edcd1cb270b805b673f3d6651ea1d47
Ihsan Berahim <ihsanberahim@gmail.com>
2018-12-08 20:21:19 +0800

    improved the test

--- lib/main.dart
+++ lib/main.dart
@@ -6,24 +6,32 @@ import 'screens/explore/explore-screen.dart';
 import 'screens/search/search-screen.dart';
 import 'login.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'MyMart',
-    home: new ExploreScreen(),
-    onGenerateRoute: (RouteSettings settings) {
-      switch (settings.name) {
-        case ExploreScreen.routeName:
-          return new BaseSlidingRoute(
-              builder: (_) => new ExploreScreen(), settings: settings);
-        case LoginScreen.routeName:
-          return new BaseSlidingRoute(
-              builder: (_) => new LoginScreen(), settings: settings);
-        case SearchScreen.routeName:
-          return new BaseSlidingRoute(
-              builder: (_) => new SearchScreen(), settings: settings);
-      }
-      assert(false);
-    },
-  ));
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MyMart',
+      home: new ExploreScreen(),
+      onGenerateRoute: (RouteSettings settings) {
+        switch (settings.name) {
+          case ExploreScreen.routeName:
+            return new BaseSlidingRoute(
+                builder: (_) => new ExploreScreen(), settings: settings);
+          case LoginScreen.routeName:
+            return new BaseSlidingRoute(
+                builder: (_) => new LoginScreen(), settings: settings);
+          case SearchScreen.routeName:
+            return new BaseSlidingRoute(
+                builder: (_) => new SearchScreen(), settings: settings);
+        }
+        assert(false);
+      },
+    );
+  }
 }
